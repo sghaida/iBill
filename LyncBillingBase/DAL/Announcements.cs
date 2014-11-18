ef42733ad7403134d77a7dea75e1a3206d1865d4
Ahmad Alhour <a.z.alhour@gmail.com>
2014-11-18 10:22:37 +0200

    Added the DbColumnAttribute class.

--- LyncBillingBase/DAL/Announcements.cs
+++ LyncBillingBase/DAL/Announcements.cs
@@ -8,21 +8,25 @@ using LyncBillingBase.Helpers;
 
 namespace LyncBillingBase.DAL
 {
-
     [TableName("Announcements")]
     public class Announcements
     {
         [IsIdField]
+        [DbColumnAttribute("ID")]
         public int ID { get; set; }
-        
+
+        [DbColumnAttribute("Announcement")]
         public string Announcement { get; set; }
         
         [IsAllowNull]
+        [DbColumnAttribute("ForRole")]
         public int ForRole { get; set; }
         
         [IsAllowNull]
+        [DbColumnAttribute("ForSite")]
         public int ForSite { get; set; }
-        
+
+        [DbColumnAttribute("PublishOn")]
         public DateTime PublishOn { get; set; }
     }
 }
