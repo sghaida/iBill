aabe68886c5fa90d7d6307acb8f1c5e5784c61dc
Saddam Abu Ghaida <saddam.abughaida@gmail.com>
2014-11-20 09:23:11 +0200

    update Names in the db functions for the positional param from db functions attributes

--- LyncBillingBase/DAL/Functions/ChargableCallsPerUser.cs
+++ LyncBillingBase/DAL/Functions/ChargableCallsPerUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LyncBillingBase.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace LyncBillingBase.DAL.Functions
 {
+    [FunctionName("Get_ChargeableCalls_ForUser")]
     class ChargableCallsPerUser : PhoneCall
     {
+        [FunctionsParameters(ParamerterName = "ChargingParty", Position=1)]
+        string ChargingParty { set; get; } 
     }
 }
