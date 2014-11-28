a66ac4ff411e4892f71745a79afc82f108b86300
Ahmad Alhour <a.z.alhour@gmail.com>
2014-11-28 12:29:40 +0200

    Moved the roles classes from the Roles directory to the DataModels directory. Modified the AnnouncementsDataMapper to read the Roles Info from the Role class.

---
+++ LyncBillingBase/DataModels/SystemRole.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LyncBillingBase.Roles
+{
+    public class SystemRole
+    {
+        public int ID { set; get; }
+        public string SipAccount { get; set; }
+        public int RoleID { get; set; }
+        public int SiteID { get; set; }
+        public string Description { get; set; }
+
+        //The following are logical representation of existing data, they don't belong to the table
+        public string SiteName { get; set; }
+        public string RoleOwnerName { get; set; }
+        public string RoleDescription { get; set; }
+
+        //"This" System Role Flags
+        public bool IsDeveloper() { return this.RoleID == DeveloperRoleID ? true : false; }
+        public bool IsSystemAdmin() { return this.RoleID == SystemAdminRoleID ? true : false; }
+        public bool IsSiteAdmin() { return this.RoleID == SiteAdminRoleID ? true : false; }
+        public bool IsSiteAccountant() { return this.RoleID == SiteAccountantRoleID ? true : false; }
+    }
+}
