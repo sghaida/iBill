64ca7c3d259d2603b342c6f7fee7302cc17d91ea
Ahmad Alhour <a.z.alhour@gmail.com>
2014-12-01 10:44:43 +0200

    Added SQLQueries namespace to the DataMappers. Renamed GatewayDetail to GatewayInfo. Renamed DataAccess#GetAllWithRelations to GetWithRelations. Fixed a bug in the DBLib#SELECT_WITH_JOIN function - where conditions parsing.

--- LyncBillingBase/DataMappers/SQLQueries/PhoneCallsSQL.cs
+++ LyncBillingBase/DataMappers/SQLQueries/PhoneCallsSQL.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LyncBillingBase.DataMappers
+namespace LyncBillingBase.DataMappers.SQLQueries
 {
     public class PhoneCallsSQL
     {
