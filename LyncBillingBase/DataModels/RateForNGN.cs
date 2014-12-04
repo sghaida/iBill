ab7b68879677f3c93dff8a7764cdfb8fbc8452bf
Ahmad Alhour <a.z.alhour@gmail.com>
2014-12-04 16:24:29 +0200

    Finished the RatesForNGN Data Mapper.

---
+++ LyncBillingBase/DataModels/RateForNGN.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LyncBillingBase.DataAccess;
+using LyncBillingBase.DataAttributes;
+
+namespace LyncBillingBase.DataModels
+{
+    [DataSource(Name = "GatewaysDetails", Type = GLOBALS.DataSource.Type.DBTable, AccessMethod = GLOBALS.DataSource.AccessMethod.DistributedSource)]
+    public class RateForNGN : DataModel
+    {
+        [IsIDField]
+        [DbColumn("RateID")]
+        public int RateID { get; set; }
+
+        [DbColumn("DialingCodeID")]
+        public Int64 DialingCodeID { get; set; }
+
+        [DbColumn("Rate")]
+        public decimal Rate { get; set; }
+
+
+        //
+        // Relations
+        [DataRelation(WithDataModel = typeof(NumberingPlanForNGN), OnDataModelKey = "ID", ThisKey = "DialingCodeID")]
+        public NumberingPlanForNGN NumberingPlanForNGN { get; set; }
+    }
+}
