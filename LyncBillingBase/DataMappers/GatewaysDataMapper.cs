aab6e19c8846e0e02aa89b1bb151f167ceee5b94
Ahmad Alhour <a.z.alhour@gmail.com>
2014-12-04 12:26:17 +0200

    Added a gateways data mapper.

--- LyncBillingBase/DataMappers/GatewaysDataMapper.cs
+++ LyncBillingBase/DataMappers/GatewaysDataMapper.cs
@@ -10,94 +10,9 @@ using LyncBillingBase.DataModels;
 
 namespace LyncBillingBase.DataMappers
 {
-    public class GatewaysDataMapper : DataAccess<GatewayInfo>
+    public class GatewaysDataMapper
     {
-        /// <summary>
-        /// Returns all the Gateways
-        /// </summary>
-        /// <returns>List of Gateway objects.</returns>
-        public List<Gateway> GetAllGateways()
-        {
-            List<Gateway> gateways = null;
-            List<GatewayInfo> gatewaysInfo = null;
-
-            try
-            {
-                gatewaysInfo = GetAll().ToList<GatewayInfo>();
-
-                if (gatewaysInfo != null && gatewaysInfo.Count > 0)
-                {
-                    gateways = gatewaysInfo.Select<GatewayInfo, Gateway>(item => item.Gateway).ToList<Gateway>();
-                }
-
-                return gateways;
-            }
-            catch (Exception ex)
-            {
-                throw ex.InnerException;
-            }
-        }
-
-
-        /// <summary>
-        /// Given a Site's ID, return the list of Gateways mapped to it.
-        /// </summary>
-        /// <param name="SiteID">Site.ID (int)</param>
-        /// <returns>List of Gateway objects.</returns>
-        public List<Gateway> GetGatewaysBySiteID(int SiteID)
-        {
-            List<Gateway> gateways = null;
-            List<GatewayInfo> gatewaysInfo = null;
-
-            Dictionary<string, object> conditions = new Dictionary<string, object>();
-            conditions.Add("SiteID", SiteID);
-
-            try
-            {
-                gatewaysInfo = Get(whereConditions: conditions, limit: 0).ToList<GatewayInfo>();
-
-                if(gatewaysInfo != null && gatewaysInfo.Count > 0)
-                { 
-                    gateways = gatewaysInfo.Select<GatewayInfo, Gateway>(item => item.Gateway).ToList<Gateway>();
-                }
-
-                return gateways;
-            }
-            catch(Exception ex)
-            {
-                throw ex.InnerException;
-            }
-        }
-
-
-        /// <summary>
-        /// Given a Gateway's ID, return the list of Sites it is associated with.
-        /// </summary>
-        /// <param name="GatewayID">Gateway.ID (int), GatewayInfo.GatewayID (int)</param>
-        /// <returns></returns>
-        public List<Site> GetSitesByGatewayID(int GatewayID)
-        {
-            List<Site> sites = null;
-            List<GatewayInfo> gatewaysInfo = null;
-
-            Dictionary<string, object> conditions = new Dictionary<string, object>();
-            conditions.Add("GatewayID", GatewayID);
-
-            try
-            {
-                gatewaysInfo = Get(whereConditions: conditions, limit: 0).ToList<GatewayInfo>();
-
-                if (gatewaysInfo != null && gatewaysInfo.Count > 0)
-                {
-                    sites = gatewaysInfo.Select<GatewayInfo, Site>(item => item.Site).ToList<Site>();
-                }
-
-                return sites;
-            }
-            catch (Exception ex)
-            {
-                throw ex.InnerException;
-            }
-        }
+        
     }
+
 }
