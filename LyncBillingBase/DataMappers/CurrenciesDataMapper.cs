5defec723498b29d1b26f9653b8ad2aeb864b9b9
Ahmad Alhour <a.z.alhour@gmail.com>
2015-01-05 15:13:10 +0200

    Finished the Countries, GatewaysRates, MonitoringServers, and Currencies Data Mappers.

--- LyncBillingBase/DataMappers/CurrenciesDataMapper.cs
+++ LyncBillingBase/DataMappers/CurrenciesDataMapper.cs
@@ -12,6 +12,24 @@ namespace LyncBillingBase.DataMappers
 {
     public class CurrenciesDataMapper : DataAccess<Currency>
     {
+        private List<Currency> _Currencies = new List<Currency>();
+
+
+        public CurrenciesDataMapper()
+        {
+            LoadCurrencies();
+        }
+
+
+        private void LoadCurrencies()
+        {
+            if(_Currencies == null || _Currencies.Count == 0)
+            {
+                _Currencies = base.GetAll().ToList();
+            }
+        }
+
+
         public Currency GetByISO3Code(string ISO3Code)
         {
             Currency currency = null;
@@ -33,6 +51,70 @@ namespace LyncBillingBase.DataMappers
             catch(Exception ex)
             {
                 throw ex.InnerException;
+            }
+        }
+
+
+        public override IEnumerable<Currency> GetAll(string dataSourceName = null, GLOBALS.DataSource.Type dataSourceType = GLOBALS.DataSource.Type.Default)
+        {
+            return _Currencies;
+        }
+
+
+        public override int Insert(Currency dataObject, string dataSourceName = null, GLOBALS.DataSource.Type dataSourceType = GLOBALS.DataSource.Type.Default)
+        {
+            bool isContained = _Currencies.Contains(dataObject);
+            bool itExists = _Currencies.Exists(
+                item =>
+                    item.Name == dataObject.Name ||
+                    item.ISO3Code == dataObject.ISO3Code
+                );
+
+            if(isContained || itExists)
+            {
+                return -1;
+            }
+            else
+            {
+                dataObject.ID = base.Insert(dataObject, dataSourceName, dataSourceType);
+                _Currencies.Add(dataObject);
+
+                return dataObject.ID;
+            }
+        }
+
+
+        public override bool Update(Currency dataObject, string dataSourceName = null, GLOBALS.DataSource.Type dataSourceType = GLOBALS.DataSource.Type.Default)
+        {
+            var currency = _Currencies.Find(item => item.ID == dataObject.ID);
+
+            if(currency != null)
+            {
+                _Currencies.Remove(currency);
+                _Currencies.Add(dataObject);
+
+                return base.Update(dataObject, dataSourceName, dataSourceType);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public override bool Delete(Currency dataObject, string dataSourceName = null, GLOBALS.DataSource.Type dataSourceType = GLOBALS.DataSource.Type.Default)
+        {
+            var currency = _Currencies.Find(item => item.ID == dataObject.ID);
+
+            if (currency != null)
+            {
+                _Currencies.Remove(currency);
+
+                return base.Delete(dataObject, dataSourceName, dataSourceType);
+            }
+            else
+            {
+                return false;
             }
         }
 
