0a910ee8c9e585dfef999e52f5884ac7f703e937
Ahmad Alhour <a.z.alhour@gmail.com>
2015-01-28 09:07:50 +0200

    Finished the Elevate Access nav-bar menu. Moved the construction of ElevateAccess and SwitchToDelegee Dropdowns to the code-behind of the Site.Master

--- LyncBillingUI/Site.Master.cs
+++ LyncBillingUI/Site.Master.cs
@@ -11,7 +11,7 @@ namespace LyncBillingUI
 {
     public partial class SiteMaster : MasterPage
     {
-        public UserSession current_session { get; set; }
+        public UserSession CurrentSession { get; set; }
         public string HTML_SELECTED = string.Empty;
         public string PAGE_NAME = string.Empty;
 
@@ -20,6 +20,9 @@ namespace LyncBillingUI
 
         //public variable made available for the view
         public string DisplayName = string.Empty;
+
+        public string UiElevateAccessDropdown = string.Empty;
+        public string UiSwtichToDelegeeDropdown = string.Empty;
 
 
         protected void Page_Load(object sender, EventArgs e)
@@ -33,7 +36,7 @@ namespace LyncBillingUI
             InitializeTheGlobalApplicationUrl();
 
             // get the current session
-            current_session = (UserSession)HttpContext.Current.Session.Contents["UserData"];
+            CurrentSession = (UserSession)HttpContext.Current.Session.Contents["UserData"];
 
             // the HTML selected class
             HTML_SELECTED = "class='active'";
@@ -51,6 +54,241 @@ namespace LyncBillingUI
 
             // format the User's effective display name
             DisplayName = GetEffectiveDisplayName();
+
+            //
+            // Get the Ui Dropdown Menus
+            ConstructUiDropdownMenus();
+        }
+
+
+        //
+        // Constructs the UiElevateAccessDropdown and UiSwtichToDelegeeDropdown Menus for the Navbar.
+        private void ConstructUiDropdownMenus()
+        {
+            //
+            // THE SWTICH TO DELEGEE DROPDOWN
+            bool swtichToDelegee = 
+                CurrentSession != null && 
+                CurrentSession.IsDelegee && 
+                (CurrentSession.ActiveRoleName == normalUserRoleName);
+
+            if (true == swtichToDelegee)
+            {
+                //
+                // The opening tags of the dropdown
+                UiSwtichToDelegeeDropdown = String.Format(
+                    "<li id='swtich-delegee-menu-tab' class='dropdown'>" + 
+                    "<a href='#' class='dropdown-toggle ibill-nav-dropdown' data-toggle='dropdown' role='button' aria-expanded='false'>Switch to User&nbsp;<span class='caret'></span></a>" + 
+                    "<ul class='dropdown-menu' role='menu'>");
+                
+                //
+                // User Delegees
+                if(CurrentSession.UserDelegateRoles.Any())
+                {
+                    UiSwtichToDelegeeDropdown = String.Format(
+                            "{0}" + 
+                            "<li class='dropdown-header'>Users</li>"
+                        , UiSwtichToDelegeeDropdown);
+
+                    foreach (var userDelegeeRecord in CurrentSession.UserDelegateRoles)
+                    {
+                        UiSwtichToDelegeeDropdown = String.Format(
+                            "{0}" + 
+                            "<li>" + 
+                            "<a href='{1}/Authorize?access=userdelegee&identity={2}'>" + 
+                            "{3}" + 
+                            "</a>" + 
+                            "</li>"
+                        , UiSwtichToDelegeeDropdown
+                        , Global.APPLICATION_URL
+                        , userDelegeeRecord.ManagedUserSipAccount
+                        , userDelegeeRecord.ManagedUser.DisplayName);
+                    }
+                }
+
+                //
+                // Divider
+                if (CurrentSession.UserDelegateRoles.Any() && CurrentSession.DepartmentDelegateRoles.Any()) 
+                {
+                    UiSwtichToDelegeeDropdown = String.Format(
+                            "{0}" + 
+                            "<li class='divider'></li>"
+                        , UiSwtichToDelegeeDropdown);
+                }
+                
+                //
+                // Department Delegees
+                if(CurrentSession.DepartmentDelegateRoles.Any())
+                {
+                    UiSwtichToDelegeeDropdown = String.Format(
+                            "{0}" +
+                            "<li class='dropdown-header'>Departments</li>"
+                        , UiSwtichToDelegeeDropdown);
+
+                    foreach (var departmentDelegeeRecord in CurrentSession.DepartmentDelegateRoles)
+                    {
+                        UiSwtichToDelegeeDropdown = String.Format(
+                                "{0}" +
+                                "<li>" + 
+                                "<a href='{1}/Authorize?access=depdelegee&identity={2}_{3}'>" + 
+                                "{2} - {3}" + 
+                                "</a>" + 
+                                "</li>"
+                            , UiSwtichToDelegeeDropdown
+                            , Global.APPLICATION_URL
+                            , departmentDelegeeRecord.ManagedSiteDepartment.Site.Name
+                            , departmentDelegeeRecord.ManagedSiteDepartment.Department.Name);
+                    }
+                }
+
+                //
+                // Divider
+                if (CurrentSession.DepartmentDelegateRoles.Any() && CurrentSession.SiteDelegateRoles.Any())
+                {
+                    UiSwtichToDelegeeDropdown = String.Format(
+                            "{0}" +
+                            "<li class='divider'></li>"
+                        , UiSwtichToDelegeeDropdown);
+                }
+
+                //
+                // Site Delegees
+                if (CurrentSession.SiteDelegateRoles.Any())
+                {
+                    UiSwtichToDelegeeDropdown = String.Format(
+                            "{0}" +
+                            "<li class='dropdown-header'>Sites</li>"
+                        , UiSwtichToDelegeeDropdown);
+
+                    foreach (var siteDelegeeRecord in CurrentSession.SiteDelegateRoles)
+                    {
+                        UiSwtichToDelegeeDropdown = String.Format(
+                                "{0}" +
+                                "<li>" + 
+                                "<a href='{1}/Authorize?access=sitedelegee&identity={2}'>" + 
+                                "{2}" + 
+                                "</a>" + 
+                                "</li>"
+                            , UiSwtichToDelegeeDropdown
+                            , Global.APPLICATION_URL
+                            , siteDelegeeRecord.ManagedSite.Name);
+                    }
+                }
+                
+                //
+                // The closing tags of the dropdown
+                UiSwtichToDelegeeDropdown = String.Format(
+                        "{0}" +
+                        "</ul>" + 
+                        "</li>"
+                    , UiSwtichToDelegeeDropdown);
+            }
+
+
+            //
+            // THE ELEVATED ACCESS MENU
+            bool elevateAccess =
+                (CurrentSession != null) &&
+                (
+                    CurrentSession.IsDeveloper || 
+                    CurrentSession.IsSystemAdmin || 
+                    CurrentSession.IsSiteAdmin || 
+                    CurrentSession.IsSiteAccountant || 
+                    CurrentSession.IsDepartmentHead
+                ) && 
+                (CurrentSession.ActiveRoleName == normalUserRoleName);
+
+            if (true == elevateAccess)
+            {
+                //
+                // The opening tags of the dropdown menu
+                UiElevateAccessDropdown = String.Format(
+                    "<li id='elevate-access-menu-tab' class='dropdown'>" +
+                    "<a href='#' class='dropdown-toggle ibill-nav-dropdown' data-toggle='dropdown' role='button' aria-expanded='false'>Elevate Access&nbsp;<span class='caret'></span></a>" +
+                    "<ul class='dropdown-menu' role='menu'>");
+
+                //
+                // System Admin role
+                if (CurrentSession.IsSystemAdmin || CurrentSession.IsDeveloper)
+                {
+                    UiElevateAccessDropdown = String.Format(
+                            "{0}" +
+                            "<li class='dropdown-header'>System Level</li>" +
+                            "<li><a title='Elevate Access to System Admin Role' href='{1}/Authorize?access=sysadmin'>System Administrator</a></li>"
+                        , UiElevateAccessDropdown
+                        , Global.APPLICATION_URL);
+                }
+
+                //
+                // Divider
+                if ((CurrentSession.IsSystemAdmin && CurrentSession.IsSiteAdmin) || CurrentSession.IsDeveloper)
+                {
+                    UiElevateAccessDropdown = String.Format(
+                            "{0}" +
+                            "<li class='divider'></li>"
+                        , UiElevateAccessDropdown);
+                }
+
+                //
+                // List section header for the Site Level
+                if ((CurrentSession.IsSiteAdmin || CurrentSession.IsSiteAccountant) || CurrentSession.IsDeveloper)
+                {
+                    UiElevateAccessDropdown = String.Format(
+                            "{0}" +
+                            "<li class='dropdown-header'>Site Level</li>"
+                        , UiElevateAccessDropdown);
+                }
+
+                //
+                // Site Admin
+                if (CurrentSession.IsSiteAdmin || CurrentSession.IsDeveloper)
+                {
+                    UiElevateAccessDropdown = String.Format(
+                            "{0}" +
+                            "<li><a title='Elevate Access to Site Administrator Role' href='{1}/Authorize?access=admin'>Site Administrator</a></li>"
+                        , UiElevateAccessDropdown
+                        , Global.APPLICATION_URL);
+                }
+
+                //
+                // Site Admin
+                if (CurrentSession.IsSiteAccountant || CurrentSession.IsDeveloper)
+                {
+                    UiElevateAccessDropdown = String.Format(
+                            "{0}" +
+                            "<li><a title='Elevate Access to Site Accountant Role' href='{1}/Authorize?access=accounting'>Site Accountant</a></li>"
+                        , UiElevateAccessDropdown
+                        , Global.APPLICATION_URL);
+                }
+
+                //
+                // Divider
+                if ((CurrentSession.IsSiteAccountant && CurrentSession.IsDepartmentHead) || CurrentSession.IsDeveloper)
+                {
+                    UiElevateAccessDropdown = String.Format(
+                            "{0}" +
+                            "<li class='divider'></li>"
+                        , UiElevateAccessDropdown);
+                }
+
+                if (CurrentSession.IsDepartmentHead || CurrentSession.IsDeveloper)
+                {
+                    UiElevateAccessDropdown = String.Format(
+                            "{0}" +
+                            "<li class='dropdown-header'>Department Level</li>" +
+                            "<li><a title='Elevate Access to Department Head Role' href='{1}/Authorize?access=dephead'>Department Head</a></li>"
+                        , UiElevateAccessDropdown
+                        , Global.APPLICATION_URL);
+                }
+
+                //
+                // Closing tags of the dropdown menu
+                UiElevateAccessDropdown = String.Format(
+                        "{0}" +
+                        "</ul>" +
+                        "</li>"
+                    , UiElevateAccessDropdown);
+            }//end-elevated-access-menu
         }
 
 
