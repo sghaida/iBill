c1745bffcd06c522a332b2dca6e0ccec9f90445f
Ahmad Alhour <a.z.alhour@gmail.com>
2015-01-29 16:04:39 +0200

    Removed jQuery and Bootstrap from NuGET, installed jQuery manually, and removed the Microsoft.Web.UI controls which bundles with ScriptManager.

--- LyncBillingUI/Site.Master.designer.cs
+++ LyncBillingUI/Site.Master.designer.cs
@@ -47,5 +47,14 @@ namespace LyncBillingUI {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        
+        /// <summary>
+        /// EndOfBodyScripts control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder EndOfBodyScripts;
     }
 }
