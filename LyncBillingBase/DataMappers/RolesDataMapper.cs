daf6b8c7c8377f1aa0e739612107ab20278e5cdf
Ahmad Alhour <a.z.alhour@gmail.com>
2015-04-02 13:25:19 +0300

    Refactored the data mappers with internal caches, and introduced locking of the internal cache upon insert, update and delete procedures.

--- LyncBillingBase/DataMappers/RolesDataMapper.cs
+++ LyncBillingBase/DataMappers/RolesDataMapper.cs
@@ -25,50 +25,59 @@ namespace LyncBillingBase.DataMappers
         {
             if (_roles == null || _roles.Count == 0)
             {
-                _roles = base.GetAll().ToList();
+                lock (_roles)
+                {
+                    _roles = base.GetAll().ToList();
 
-                // Developer Role
-                var developer = _roles.Find(item => item.RoleName.ToLower() == "developer");
-                DeveloperRoleID = (developer != null ? developer.RoleId : -1);
+                    // Developer Role
+                    var developer = _roles.Find(item => item.RoleName.ToLower() == "developer");
+                    DeveloperRoleID = (developer != null ? developer.RoleId : -1);
 
-                // System Admin Role
-                var systemAdmin = _roles.Find(item => item.RoleName.ToLower() == "sysadmin");
-                SystemAdminRoleID = (systemAdmin != null ? systemAdmin.RoleId : -1);
+                    // System Admin Role
+                    var systemAdmin = _roles.Find(item => item.RoleName.ToLower() == "sysadmin");
+                    SystemAdminRoleID = (systemAdmin != null ? systemAdmin.RoleId : -1);
 
-                // Site Admin Role
-                var siteAdmin = _roles.Find(item => item.RoleName.ToLower() == "admin");
-                SiteAdminRoleID = (siteAdmin != null ? siteAdmin.RoleId : -1);
+                    // Site Admin Role
+                    var siteAdmin = _roles.Find(item => item.RoleName.ToLower() == "admin");
+                    SiteAdminRoleID = (siteAdmin != null ? siteAdmin.RoleId : -1);
 
-                // Site Accountant Role
-                var siteAccountant = _roles.Find(item => item.RoleName.ToLower() == "accounting");
-                SiteAccountantRoleID = (siteAccountant != null ? siteAccountant.RoleId : -1);
+                    // Site Accountant Role
+                    var siteAccountant = _roles.Find(item => item.RoleName.ToLower() == "accounting");
+                    SiteAccountantRoleID = (siteAccountant != null ? siteAccountant.RoleId : -1);
 
-                // Department Head Role
-                var departmentHead = _roles.Find(item => item.RoleName.ToLower() == "dephead");
-                DepartmentHeadRoleID = (departmentHead != null ? departmentHead.RoleId : -1);
+                    // Department Head Role
+                    var departmentHead = _roles.Find(item => item.RoleName.ToLower() == "dephead");
+                    DepartmentHeadRoleID = (departmentHead != null ? departmentHead.RoleId : -1);
 
-                // Site Delegee Role
-                var siteDelegee = _roles.Find(item => item.RoleName.ToLower() == "sitedelegee");
-                SiteDelegeeRoleID = (siteDelegee != null ? siteDelegee.RoleId : -1);
+                    // Site Delegee Role
+                    var siteDelegee = _roles.Find(item => item.RoleName.ToLower() == "sitedelegee");
+                    SiteDelegeeRoleID = (siteDelegee != null ? siteDelegee.RoleId : -1);
 
-                // Department Delegee Role
-                var departmentDelegee = _roles.Find(item => item.RoleName.ToLower() == "depdelegee");
-                DepartmentDelegeeRoleID = (departmentDelegee != null ? departmentDelegee.RoleId : -1);
+                    // Department Delegee Role
+                    var departmentDelegee = _roles.Find(item => item.RoleName.ToLower() == "depdelegee");
+                    DepartmentDelegeeRoleID = (departmentDelegee != null ? departmentDelegee.RoleId : -1);
 
-                // User Delegee Role
-                var userDelegee = _roles.Find(item => item.RoleName.ToLower() == "userdelegee");
-                UserDelegeeRoleID = (userDelegee != null ? userDelegee.RoleId : -1);
+                    // User Delegee Role
+                    var userDelegee = _roles.Find(item => item.RoleName.ToLower() == "userdelegee");
+                    UserDelegeeRoleID = (userDelegee != null ? userDelegee.RoleId : -1);
 
-                // Normal User Role
-                var user = _roles.Find(item => item.RoleName.ToLower() == "user");
-                UserRoleID = (user != null ? user.RoleId : -1);
-            }
+                    // Normal User Role
+                    var user = _roles.Find(item => item.RoleName.ToLower() == "user");
+                    UserRoleID = (user != null ? user.RoleId : -1);
+
+                }//end-lock
+            }//end-if
         }
 
+
+        /// <summary>
+        /// CONSTRUCTOR
+        /// </summary>
         public RolesDataMapper()
         {
             LoadRoles();
         }
+
 
         /// <summary>
         /// </summary>
@@ -78,13 +87,14 @@ namespace LyncBillingBase.DataMappers
         {
             try
             {
-                return _roles.FirstOrDefault(item => item.RoleId == roleId);
+                return _roles.Find(item => item.RoleId == roleId);
             }
             catch (Exception ex)
             {
                 throw ex.InnerException;
             }
         }
+
 
         /// <summary>
         /// 
@@ -97,7 +107,7 @@ namespace LyncBillingBase.DataMappers
 
             try
             {
-                var role = _roles.FirstOrDefault(item => item.RoleId == roleId);
+                var role = _roles.Find(item => item.RoleId == roleId);
 
                 if(role != null)
                 {
@@ -112,52 +122,169 @@ namespace LyncBillingBase.DataMappers
             }
         }
 
+
         public override IEnumerable<Role> GetAll(string dataSourceName = null, Globals.DataSource.Type dataSourceType = Globals.DataSource.Type.Default)
         {
             return _roles;
         }
 
+
         public override int Insert(Role dataObject, string dataSourceName = null, Globals.DataSource.Type dataSourceType = Globals.DataSource.Type.Default)
         {
+            //
+            // Null Checks
+            if(dataObject == null)
+            {
+                return -1;
+            }
+
+            //
+            // Containment and existence checks
             var isContained = _roles.Contains(dataObject);
-            var itExists =
-                _roles.Exists(item => item.RoleId == dataObject.RoleId && item.RoleName == dataObject.RoleName);
+            var itExists = _roles.Exists(item => item.RoleId == dataObject.RoleId && item.RoleName == dataObject.RoleName);
 
             if (isContained || itExists)
             {
                 return -1;
             }
-            dataObject.Id = base.Insert(dataObject, dataSourceName, dataSourceType);
-            _roles.Add(dataObject);
+
+            //
+            // Insert process
+            try
+            {
+                dataObject.Id = base.Insert(dataObject, dataSourceName, dataSourceType);
+                
+                lock(_roles)
+                {
+                    _roles.Add(dataObject);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex.InnerException;
+            }
 
             return dataObject.Id;
         }
 
+
         public override bool Update(Role dataObject, string dataSourceName = null, Globals.DataSource.Type dataSourceType = Globals.DataSource.Type.Default)
         {
+            bool status = false;
+
+            //
+            // Null check
+            if(dataObject == null)
+            {
+                return status;
+            }
+
+            //
+            // Existence check
             var role = _roles.Find(item => item.Id == dataObject.Id);
 
             if (role != null)
             {
-                _roles.Remove(role);
-                _roles.Add(dataObject);
+                try
+                {
+                    status = base.Update(dataObject, dataSourceName, dataSourceType);
 
-                return base.Update(dataObject, dataSourceName, dataSourceType);
+                    if (status == true)
+                    {
+                        lock (_roles)
+                        {
+                            _roles.Remove(role);
+                            _roles.Add(dataObject);
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    throw ex.InnerException;
+                }
             }
-            return false;
+
+            return status;
         }
+
 
         public override bool Delete(Role dataObject, string dataSourceName = null, Globals.DataSource.Type dataSourceType = Globals.DataSource.Type.Default)
         {
+            bool status = false;
+
+            //
+            // Null check
+            if(dataObject == null)
+            {
+                return status;
+            }
+
+            //
+            // Existence check
             var role = _roles.Find(item => item.Id == dataObject.Id);
 
             if (role != null)
             {
-                _roles.Remove(role);
+                try
+                {
+                    status = base.Delete(dataObject, dataSourceName, dataSourceType);
 
-                return base.Delete(dataObject, dataSourceName, dataSourceType);
+                    if(status == true)
+                    {
+                        lock (_roles)
+                        {
+                            _roles.Remove(role);
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    throw ex.InnerException;
+                }
             }
             return false;
         }
+
+
+        /***
+         * DISABLED FUNCTIONS
+         */
+        [Obsolete]
+        public override IEnumerable<Role> Get(Dictionary<string, object> whereConditions, int limit = 25, string dataSourceName = null, CCC.ORM.Globals.DataSource.Type dataSourceType = CCC.ORM.Globals.DataSource.Type.Default)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Obsolete]
+        public override IEnumerable<Role> Get(System.Linq.Expressions.Expression<Func<Role, bool>> predicate, string dataSourceName = null, CCC.ORM.Globals.DataSource.Type dataSourceType = CCC.ORM.Globals.DataSource.Type.Default)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Obsolete]
+        public override IEnumerable<Role> GetAll(string sqlQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Obsolete]
+        public override int Insert(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Obsolete]
+        public override bool Update(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Obsolete]
+        public override bool Delete(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
     }
+
 }
---
+++ LyncBillingUI/Pages/SiteAccounting/DisputedCalls.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using LyncBillingBase.DataModels;
+using LyncBillingUI.Account;
+
+namespace LyncBillingUI.Pages.SiteAccounting
+{
+    public partial class DisputedCalls : System.Web.UI.Page
+    {
+        private string sipAccount = string.Empty;
+
+        private static string normalUserRoleName { get; set; }
+        private static string userDelegeeRoleName { get; set; }
+        private static string allowedRoleName { get; set; }
+
+        // This actually takes a copy of the current session for some uses on the frontend.
+        public UserSession CurrentSession { get; set; }
+
+        private static List<Site> userSites;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //
+            // Set the role names of User and Delegee
+            SetRolesNames();
+
+            // 
+            // If the user is not loggedin, redirect to Login page.
+            if (HttpContext.Current.Session == null || HttpContext.Current.Session.Contents["UserData"] == null)
+            {
+                string RedirectTo = String.Format(@"{0}/User/PhoneCalls", Global.APPLICATION_URL);
+                string Url = String.Format(@"{0}/Login?RedirectTo={1}", Global.APPLICATION_URL, RedirectTo);
+                Response.Redirect(Url);
+            }
+            else
+            {
+                CurrentSession = ((UserSession)HttpContext.Current.Session.Contents["UserData"]);
+                if (CurrentSession.ActiveRoleName != normalUserRoleName && CurrentSession.ActiveRoleName != userDelegeeRoleName)
+                {
+                    string url = String.Format(@"{0}/Authenticate?access={1}", Global.APPLICATION_URL, CurrentSession.ActiveRoleName);
+                    Response.Redirect(url);
+                }
+            }
+
+            sipAccount = CurrentSession.GetEffectiveSipAccount();
+
+            GetUserSitesData();
+        }
+
+
+        //
+        // Set the role names of User and Delegee
+        private void SetRolesNames()
+        {
+            if (string.IsNullOrEmpty(normalUserRoleName))
+            {
+                normalUserRoleName = Global.DATABASE.Roles.GetRoleNameById(Global.DATABASE.Roles.UserRoleID);
+            }
+
+            if (string.IsNullOrEmpty(userDelegeeRoleName))
+            {
+                userDelegeeRoleName = Global.DATABASE.Roles.GetRoleNameById(Global.DATABASE.Roles.UserDelegeeRoleID);
+            }
+
+            if (string.IsNullOrEmpty(allowedRoleName))
+            {
+                allowedRoleName = Global.DATABASE.Roles.GetRoleNameById(Global.DATABASE.Roles.SiteAccountantRoleID);
+            }
+        }
+
+
+        //
+        // Get the sites data for this user
+        private void GetUserSitesData()
+        {
+            if (!Ext.Net.X.IsAjaxRequest)
+            {
+                userSites = Global.DATABASE.SystemRoles.GetSitesByRoles(CurrentSession.SystemRoles, allowedRoleName);
+            }
+        }
+
+    }
+
+}
