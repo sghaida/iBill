c19620dbc3b1a973aeeda0debd682fe444ff1c13
Ahmad Alhour <a.z.alhour@gmail.com>
2015-05-26 12:46:58 +0300

    Changed the theme of ExtNET.

--- LyncBillingUI/Pages/User/Statistics.aspx.designer.cs
+++ LyncBillingUI/Pages/User/Statistics.aspx.designer.cs
@@ -112,15 +112,6 @@ namespace LyncBillingUI.Pages.User {
         protected global::Ext.Net.Model DurationCostChartModel;
         
         /// <summary>
-        /// VerticalMarker1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Ext.Net.VerticalMarker VerticalMarker1;
-        
-        /// <summary>
         /// PhoneCallsDuartionChartPanel control.
         /// </summary>
         /// <remarks>
